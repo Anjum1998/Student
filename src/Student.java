51f7e9df238948072ba92daf98616997cbbb9d9c
Anju M <Anju@NTP-LAP-771.chn.nestgroup.net>
2022-11-21 23:40:26 +0530

    second commit

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class stud
+{
+    String name;
+    int rollNo;
+    String admissionNo;
+    String college;
+
+    public stud(String name, int rollNo, String admissionNo, String college) {
+        this.name = name;
+        this.rollNo = rollNo;
+        this.admissionNo = admissionNo;
+        this.college = college;
+    }
+}
 public class Student {
+    public static void main(String[] args) {
+        ArrayList<stud> arr=new ArrayList<stud>();
+        while(true)
+        {
+            System.out.println("Select an option \n 1.Add Student \n 2.View Student \n 3.Search Student \n 4.Delete student \n 5.Exit");
+            Scanner sc=new Scanner(System.in);
+            int n = sc.nextInt();
+            switch(n)
+            {
+
+            }
+        }
+
+    }
 }
